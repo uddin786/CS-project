096f7e3e7b66f3ffdcd8165c8a79cc0ea585801c
Mohammed Samir Uddin <f014248@beckfootoakbank.org>
2020-11-28 17:55:53 +0000

    a

---
+++ CS project/Piece.vb
@@ -0,0 +1,11 @@
+﻿Public Class Piece
+    Public x_pos, y_pos As Integer
+    Public piece_val As Integer
+
+    Public Sub New(ByVal x, ByVal y, ByVal p_value)
+        x_pos = x
+        y_pos = y
+        piece_val = p_value
+
+    End Sub
+End Class
