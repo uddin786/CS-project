5a49a494a9abdc6454a1b201e5fdf6387b1d1bb2
Mohammed Samir Uddin <f014248@beckfootoakbank.org>
2020-11-29 14:45:32 +0000

    a

--- CS project/Form2.vb
+++ CS project/Form2.vb
@@ -1,4 +1,32 @@
 ﻿Public Class Form2
+    Public board(7, 7) As Integer
+    Public P As String = ""
+    Public Sub New()
+        board = {{1, 0, 1, 0, 1, 0, 1, 0},
+                {0, 1, 0, 1, 0, 1, 0, 1},
+                {0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0},
+                {-1, 0, -1, 0, -1, 0, -1, 0},
+                {0, -1, 0, -1, 0, -1, 0, -1}}
+
+    End Sub
+
+    Public Sub Shows()
+
+        For i As Integer = 0 To 7
+            For j As Integer = 0 To 7
+                Select Case board(i, j)
+                    Case 1
+                        Me.P(i, j).image = My.Resources.g
+                    Case -1
+                        Me.P(i, j).image = My.Resources.r
+                End Select
+            Next
+        Next
+    End Sub
+
     Private Sub Form2_Load(sender As Object, e As EventArgs) Handles MyBase.Load
 
     End Sub
