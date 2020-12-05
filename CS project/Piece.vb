251c22fdfc71937ef6623e9806e1830f20886ea5
Mohammed Samir Uddin <f014248@beckfootoakbank.org>
2020-12-05 19:27:55 +0000

    a

--- CS project/Piece.vb
+++ CS project/Piece.vb
@@ -8,30 +8,4 @@
         piece_val = p_value
 
     End Sub
-
-    Public board(7, 7) As Integer
-    'Public P As String = "P"
-    Public Sub boards()
-        board = {{1, 0, 1, 0, 1, 0, 1, 0},
-                {0, 1, 0, 1, 0, 1, 0, 1},
-                {0, 0, 0, 0, 0, 0, 0, 0},
-                {0, 0, 0, 0, 0, 0, 0, 0},
-                {0, 0, 0, 0, 0, 0, 0, 0},
-                {0, 0, 0, 0, 0, 0, 0, 0},
-                {-1, 0, -1, 0, -1, 0, -1, 0},
-                {0, -1, 0, -1, 0, -1, 0, -1}}
-
-    End Sub
-    Public Sub shows()
-        For i As Integer = 0 To 7
-            For j As Integer = 0 To 7
-                Select Case board(i, j)
-                    Case 1
-                        Form2.P(i, j).image = My.Resources.g
-                    Case -1
-                        Form2.P(i, j).image = My.Resources.r
-                End Select
-            Next
-        Next
-    End Sub
 End Class
