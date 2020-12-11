6aa279c87e551a43326f9a3be5e5f30617784aa0
Mohammed Samir Uddin <f014248@beckfootoakbank.org>
2020-12-11 20:15:30 +0000

    a

--- CS project/Form3.vb
+++ CS project/Form3.vb
@@ -2,4 +2,10 @@
     Private Sub Label1_Click(sender As Object, e As EventArgs) Handles Label1.Click
 
     End Sub
+
+    Private Sub Form3_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        P11.Image = Image.FromFile("g.png")
+        P11.SizeMode = PictureBoxSizeMode.StretchImage
+
+    End Sub
 End Class
--- CS project/My Project/Resources.Designer.vb
+++ CS project/My Project/Resources.Designer.vb
@@ -73,6 +73,16 @@ Namespace My.Resources
         '''<summary>
         '''  Looks up a localized resource of type System.Drawing.Bitmap.
         '''</summary>
+        Friend ReadOnly Property g1() As System.Drawing.Bitmap
+            Get
+                Dim obj As Object = ResourceManager.GetObject("g1", resourceCulture)
+                Return CType(obj,System.Drawing.Bitmap)
+            End Get
+        End Property
+        
+        '''<summary>
+        '''  Looks up a localized resource of type System.Drawing.Bitmap.
+        '''</summary>
         Friend ReadOnly Property gk() As System.Drawing.Bitmap
             Get
                 Dim obj As Object = ResourceManager.GetObject("gk", resourceCulture)
