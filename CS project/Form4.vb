2a49c1eecd0d68c3f28df1e3273f7d053e7c2980
Mohammed Samir Uddin <f014248@beckfootoakbank.org>
2020-12-21 13:26:00 +0000

    aa

--- CS project/Form4.vb
+++ CS project/Form4.vb
@@ -1,5 +1,5 @@
 ﻿Public Class Form4
-    Dim P71red As Boolean
+    Dim P71red, P82red, P73red, P84red, P75red, P86red, P77red, P88red As Boolean
     Public Sub shows()
         P11.Image = Image.FromFile("g.png")
         P11.SizeMode = PictureBoxSizeMode.StretchImage
@@ -22,18 +22,25 @@
         P71red = True
         P73.Image = Image.FromFile("r.png")
         P73.SizeMode = PictureBoxSizeMode.StretchImage
+        P73red = True
         P75.Image = Image.FromFile("r.png")
         P75.SizeMode = PictureBoxSizeMode.StretchImage
+        P75red = True
         P77.Image = Image.FromFile("r.png")
         P77.SizeMode = PictureBoxSizeMode.StretchImage
+        P77red = True
         P82.Image = Image.FromFile("r.png")
         P82.SizeMode = PictureBoxSizeMode.StretchImage
+        P82red = True
         P84.Image = Image.FromFile("r.png")
         P84.SizeMode = PictureBoxSizeMode.StretchImage
+        P84red = True
         P86.Image = Image.FromFile("r.png")
         P86.SizeMode = PictureBoxSizeMode.StretchImage
+        P86red = True
         P88.Image = Image.FromFile("r.png")
         P88.SizeMode = PictureBoxSizeMode.StretchImage
+        P88red = True
     End Sub
 
     'Public Function selected(ByRef x)
@@ -53,30 +60,44 @@
     End Sub
 
     Private Sub P82_Click(sender As Object, e As EventArgs) Handles P82.Click
-
+        If P82red = True Then
+            P82.Image = Image.FromFile("rs.png")
+        End If
     End Sub
 
     Private Sub P73_Click(sender As Object, e As EventArgs) Handles P73.Click
-        selected(P73)
+        If P73red = True Then
+            P73.Image = Image.FromFile("rs.png")
+        End If
     End Sub
 
     Private Sub P84_Click(sender As Object, e As EventArgs) Handles P84.Click
-
+        If P84red = True Then
+            P84.Image = Image.FromFile("rs.png")
+        End If
     End Sub
 
     Private Sub P75_Click(sender As Object, e As EventArgs) Handles P75.Click
-        selected(P75)
+        If P75red = True Then
+            P75.Image = Image.FromFile("rs.png")
+        End If
     End Sub
 
     Private Sub P86_Click(sender As Object, e As EventArgs) Handles P86.Click
-
+        If P86red = True Then
+            P86.Image = Image.FromFile("rs.png")
+        End If
     End Sub
 
     Private Sub P77_Click(sender As Object, e As EventArgs) Handles P77.Click
-        selected(P77)
+        If P77red = True Then
+            P77.Image = Image.FromFile("rs.png")
+        End If
     End Sub
 
     Private Sub P88_Click(sender As Object, e As EventArgs) Handles P88.Click
-
+        If P88red = True Then
+            P88.Image = Image.FromFile("rs.png")
+        End If
     End Sub
 End Class
